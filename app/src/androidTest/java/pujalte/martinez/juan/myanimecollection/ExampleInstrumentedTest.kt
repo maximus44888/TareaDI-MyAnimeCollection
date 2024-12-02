668c7e74b61df44b4fbfb7963682581df5e0a897
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2024-12-02 15:31:52 +0100

    init commit

---
+++ app/src/androidTest/java/pujalte/martinez/juan/myanimecollection/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package pujalte.martinez.juan.myanimecollection
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("pujalte.martinez.juan.myanimecollection", appContext.packageName)
+    }
+}
