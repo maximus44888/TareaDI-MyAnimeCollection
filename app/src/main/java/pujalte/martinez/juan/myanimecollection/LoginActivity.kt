17a7a60e87a33ddb991dcfdafb0e32289d38d98b
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2024-12-04 00:36:29 +0100

    ActivityLoginBinding

--- app/src/main/java/pujalte/martinez/juan/myanimecollection/LoginActivity.kt
+++ app/src/main/java/pujalte/martinez/juan/myanimecollection/LoginActivity.kt
@@ -1,17 +1,24 @@
 package pujalte.martinez.juan.myanimecollection
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pujalte.martinez.juan.myanimecollection.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
