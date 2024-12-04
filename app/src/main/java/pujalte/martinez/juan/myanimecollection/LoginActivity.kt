3328e15fbcec623597a39485ff572a18eea56663
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2024-12-04 18:36:11 +0100

    Error in case EditText is blank

--- app/src/main/java/pujalte/martinez/juan/myanimecollection/LoginActivity.kt
+++ app/src/main/java/pujalte/martinez/juan/myanimecollection/LoginActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.textfield.TextInputLayout
 import pujalte.martinez.juan.myanimecollection.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -22,6 +24,28 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initialize(savedInstanceState)
+    }
+
+    private fun initialize(savedInstanceState: Bundle?) {
+        binding.userInputLayout.editText?.doAfterTextChanged {
+            if (it.isNullOrBlank()) {
+                binding.userInputLayout.error = getString(R.string.cant_be_empty)
+                binding.userInputLayout.isErrorEnabled = true
+            } else {
+                binding.userInputLayout.error = null
+                binding.userInputLayout.isErrorEnabled = false
+            }
+        }
+        binding.passwordInputLayout.editText?.doAfterTextChanged {
+            if (it.isNullOrBlank()) {
+                binding.passwordInputLayout.error = getString(R.string.cant_be_empty)
+                binding.passwordInputLayout.isErrorEnabled = true
+            } else {
+                binding.passwordInputLayout.error = null
+                binding.passwordInputLayout.isErrorEnabled = false
+            }
         }
     }
 }
