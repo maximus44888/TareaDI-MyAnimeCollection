eb0fb8d60d7aec56cb18a18c46f6134214bf01be
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2024-12-14 19:37:53 +0100

    ActivityContactoBinding

--- app/src/main/java/pujalte/martinez/juan/myanimecollection/ContactoActivity.kt
+++ app/src/main/java/pujalte/martinez/juan/myanimecollection/ContactoActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pujalte.martinez.juan.myanimecollection.databinding.ActivityContactoBinding
 
 class ContactoActivity : AppCompatActivity() {
+
+    private val binding: ActivityContactoBinding by lazy {
+        ActivityContactoBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_contacto)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initialize(savedInstanceState)
+    }
+
+    private fun initialize(savedInstanceState: Bundle?) {
+
     }
 }
