5d829127947e10b8072743f41ec73f5d14b677a7
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2024-12-14 21:26:26 +0100

    Created section layout to unify components. Also some littel manipulation of those to check it works

--- app/src/main/java/pujalte/martinez/juan/myanimecollection/ContactoActivity.kt
+++ app/src/main/java/pujalte/martinez/juan/myanimecollection/ContactoActivity.kt
@@ -1,11 +1,14 @@
 package pujalte.martinez.juan.myanimecollection
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pujalte.martinez.juan.myanimecollection.databinding.ActivityContactoBinding
+import pujalte.martinez.juan.myanimecollection.databinding.LayoutSectionBinding
 
 class ContactoActivity : AppCompatActivity() {
 
@@ -26,6 +29,17 @@ class ContactoActivity : AppCompatActivity() {
     }
 
     private fun initialize(savedInstanceState: Bundle?) {
+
+        fun setupSection(section: LayoutSectionBinding, @DrawableRes resId: Int, contentDescription: String, inputType: Int) {
+            section.imageView.setImageResource(resId)
+            section.imageView.contentDescription = contentDescription
+            section.editText.inputType = inputType
+        }
+
+        setupSection(binding.phone, R.drawable.icons8_google_480, "Phone", InputType.TYPE_CLASS_PHONE)
+        setupSection(binding.mail, R.drawable.icons8_facebook_500, "Mail", InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+        setupSection(binding.whatsapp, R.drawable.fav_selected, "Whatsapp", InputType.TYPE_CLASS_PHONE)
+        setupSection(binding.location, R.drawable.ic_launcher_foreground, "Location", InputType.TYPE_CLASS_TEXT)
 
     }
 }
