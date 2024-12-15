b21f05445882adcdcd00e9f28f5b49d154ca43cd
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2024-12-15 18:26:46 +0100

    commit guarro pero funciona

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/pujalte/martinez/juan/myanimecollection/ContactoActivity.kt
+++ app/src/main/java/pujalte/martinez/juan/myanimecollection/ContactoActivity.kt
@@ -1,13 +1,22 @@
 package pujalte.martinez.juan.myanimecollection
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
 import android.os.Bundle
 import android.text.InputType
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import pujalte.martinez.juan.myanimecollection.databinding.ActivityContactoBinding
 import pujalte.martinez.juan.myanimecollection.databinding.LayoutSectionBinding
 
@@ -31,10 +40,40 @@ class ContactoActivity : AppCompatActivity() {
 
     private fun initialize(savedInstanceState: Bundle?) {
 
+        val requestPermissionLauncher = registerForActivityResult(RequestPermission()) {
+                granted: Boolean ->
+            if (granted) {
+                val intent = Intent(Intent.ACTION_CALL).apply {
+                    data = Uri.parse("tel:691313272")
+                }
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+            } else {
+                Snackbar.make(binding.root, "Feature not available cause permission denied", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
         fun setupSection(section: LayoutSectionBinding, @DrawableRes resId: Int, contentDescription: String, inputType: Int) {
             section.button.icon = AppCompatResources.getDrawable(this, resId)
             section.button.contentDescription = contentDescription
             section.editText.inputType = inputType
+            section.button.setOnClickListener {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PERMISSION_GRANTED) {
+                    val intent = Intent(Intent.ACTION_CALL).apply {
+                        data = Uri.parse("tel:691313272")
+                    }
+                    if (intent.resolveActivity(packageManager) != null) {
+                        startActivity(intent)
+                    }
+                } else {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+                        Snackbar.make(binding.root, "Permission required", Snackbar.LENGTH_LONG).show()
+                    } else {
+                        requestPermissionLauncher.launch(Manifest.permission.CALL_PHONE)
+                    }
+                }
+            }
         }
 
         setupSection(binding.phone, R.drawable.phone_call, "Phone", InputType.TYPE_CLASS_PHONE)
